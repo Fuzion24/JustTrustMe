97df9d6c9c78ddd1e945f167aa9b8d82b94fe21f
Ryan Welton <ryan.g.welton@gmail.com>
2016-10-17 18:28:21 -0400

    Updates

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 APK_PATH="bin/JustTrustMe.apk"
+mkdir bin
 ./gradlew assembleRelease && cp app/build/outputs/apk/app-release-unsigned.apk $APK_PATH && signapk $APK_PATH
 
 #adb install -r bin/AndroidVTS.apk
